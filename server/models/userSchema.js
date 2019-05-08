a70312adc07b415683df113d5c3114c5865d48bb
GaddamMeghana578 <maggi.reddy84@gmail.com>
2019-05-08 14:40:27 -0400

    Storing data into MongoDB

---
+++ server/models/userSchema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"; // Helper for communicating with Mongodb.
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  age: {
+    type: Number,
+    required: true
+  },
+  uuid: {
+    type: String,
+    required: true
+  }
+});
+export default mongoose.model("User", UserSchema);
--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 import express from "express"; // Reference express for middleware
 import { ApolloServer, gql } from "apollo-server-express";
-
-const userData = [
-  { name: "John", age: 22, uuid: "6717d7cf-6386-4775-a226-1c8b68013b01" },
-  { name: "Mike", age: 56, uuid: "5717d7cf-6386-4775-a226-1c8b68013b02" }
-];
+import mongoose from "mongoose"; // Helper for communicating with Mongodb.
+import User from "./models/userSchema"; // Reference to userSchema.js
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
@@ -19,8 +16,20 @@ const typeDefs = gql`
     users: [User!]
     user(uuid: String!): User
   }
+  input addUserInput {
+    name: String!
+    age: Int!
+    uuid: String!
+  }
+  input updateUserInput {
+    name: String
+    age: Int
+    uuid: String
+  }
   type Mutation {
-    addUser(name: String!, age: Int!, uuid: String!): User
+    addUser(input: addUserInput): User
+    updateUser(uuid: String!, input: updateUserInput): User
+    deleteUser(uuid: String!): User
   }
   schema {
     query: Query
@@ -32,18 +41,20 @@ const typeDefs = gql`
 // schema.
 const resolvers = {
   Query: {
-    users: () => {
-      return Object.values(userData);
-    },
-    user: (parent, { uuid }) => {
-      return users[uuid];
-    }
+    users: async () => await User.find({}).exec(),
+    user: async (_, { uuid }) => await User.findOne({ uuid })
   },
 
   Mutation: {
-    addUser(root, doc) {
-      userData.push(doc);
-      return doc;
+    async addUser(_, { input }) {
+      console.log("Creating User");
+      return await User.create(input);
+    },
+    async updateUser(_, { uuid, input }) {
+      return await User.findOneAndUpdate({ uuid }, input, { new: true });
+    },
+    async deleteUser(_, { uuid }) {
+      return await User.findByIdAndRemove(uuid);
     }
   }
 };
@@ -52,6 +63,13 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 app.use(express.static(__dirname + "/../client"));
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb://localhost:27017/dev", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
